f7671e409aefa76c72f45016915c944bc35640ed
Shivam <kingshivam2004@gmail.com>
2025-05-21 13:05:30 +0530

    Updated: Landing Page Added: Toast UI in Admin Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
-import React from 'react';
-import Hero from './components/landing/Hero';
-import { useNavigate } from "react-router-dom";
-import { useAuthStore } from "./store/useAuthStore";
+
+import Hero from "./components/landing/Hero";
 
 const App = () => {
-    const user = useAuthStore((state) => state.user);
-    const navigate = useNavigate();
-    const role = useAuthStore((state) => state.user?.role);
-
-    const handleClick = () => {
-        if (user) {
-            // Redirect to dashboard based on user role
-            navigate(`/${role}/dashboard`);
-        } else {
-            // Redirect to login if not logged in
-            navigate("/login");
-        }
-    };
-
     return (
-    <div className=" bg-white">
-      
-      <main>
-        <Hero />
-      </main>
-    </div>
-  );
-}
+        <div className=" bg-white">
+            <main>
+                <Hero />
+            </main>
+        </div>
+    );
+};
 
 export default App;
